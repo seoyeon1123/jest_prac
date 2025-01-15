5e8432fa7e7e04a7b48ac1e830ca9e125c10af0b
이서연 <iseoyeon@pyowonsig-ui-MacBookAir.local>
2025-01-15 17:37:40 +0900

    Feat: jest 테스트 코드 prac

--- src/__test__/Login.spec.tsx
+++ src/__test__/Login.spec.tsx
@@ -1,77 +1,66 @@
-import "@testing-library/jest-dom";
-
-import * as nock from "nock";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import * as nock from 'nock';
 import {
   fireEvent,
   render,
   renderHook,
   screen,
   waitFor,
-} from "@testing-library/react";
-import LoginPage from "../pages/LoginPage";
-import { RouterProvider, createMemoryRouter } from "react-router-dom";
-import useLogin from "../hooks/useLogin";
+} from '@testing-library/react';
+import LoginPage from '../pages/LoginPage';
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
+import useLogin from '../hooks/useLogin';
 
 const queryClient = new QueryClient({
   defaultOptions: {},
 });
 
-const wrapper = ({ children }) => (
-  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-);
-
-describe("로그인 테스트", () => {
+describe('로그인테스트', () => {
   beforeEach(() => {
-    jest.spyOn(console, "error").mockImplementation(() => {});
+    jest.spyOn(console, 'error').mockImplementation(() => {});
   });
-
   afterAll(() => {
     jest.restoreAllMocks();
   });
-
-  test("로그인에 실패하면 에러메세지가 나타난다", async () => {
-    //given - 로그인 화면이 그려진다
+  test('로그인에 실패하면 에러메세지가 나타난다', async () => {
     const routes = [
       {
-        path: "/login",
+        path: '/login',
         element: <LoginPage />,
       },
     ];
-
     const router = createMemoryRouter(routes, {
-      initialEntries: ["/login"],
+      initialEntries: ['/login'],
       initialIndex: 0,
     });
-
     render(
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
     );
 
-    // when - 사용자가 로그인에 실패한다
-    nock("https://server.byeongjinkang.com")
-      .post("/user/login/", {
-        username: "wrong@email.com",
-        password: "wrongPassword",
+    nock('https://inflearn.byeongjinkang.com')
+      .post('/user/login', {
+        username: 'wrong@email.com',
+        password: 'wrongPassword',
       })
-      .reply(400, { msg: "NO_SUCH_USER" });
+      .reply(400, { msg: 'NO_SUCH_USER' });
 
-    const emailInput = screen.getByLabelText("이메일");
-    const passwordInput = screen.getByLabelText("비밀번호");
+    const wrapper = ({ children }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    );
 
-    fireEvent.change(emailInput, { target: { value: "wrong@email.com" } });
-    fireEvent.change(passwordInput, { target: { value: "wrongPassword" } });
+    const emailInput = screen.getByLabelText('이메일');
+    const passwordInput = screen.getByLabelText('비밀번호');
 
-    const loginButton = screen.getByRole("button", { name: "로그인" });
+    fireEvent.change(emailInput, { target: { value: 'wrong@email.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'wrongPassword' } });
+
+    const loginButton = screen.getByRole('button', { name: '로그인' });
     fireEvent.click(loginButton);
 
     const { result } = renderHook(() => useLogin(), { wrapper });
 
-    // then - 에러메세지가 나타남
     await waitFor(() => result.current.isError);
-    const errorMessage = await screen.findByTestId("error-message");
-    expect(errorMessage).toBeInTheDocument();
   });
 });
--- src/__test__/Signup.spec.tsx
+++ src/__test__/Signup.spec.tsx
@@ -1,73 +1,69 @@
-import "@testing-library/jest-dom";
-
-import { fireEvent, render, screen } from "@testing-library/react";
-import SignupPage from "../pages/SignupPage";
-import { RouterProvider, createMemoryRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+//비밀번호와 비밀번호 확인의 값이 다른 경우, 에러메시지가 나타나는지
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+import SignupPage from '../pages/SignupPage';
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 
 const queryClient = new QueryClient({
   defaultOptions: {},
 });
 
-describe("회원가입 테스트", () => {
+describe('회원가입 테스트', () => {
   beforeEach(() => {
     const routes = [
       {
-        path: "/signup",
+        path: '/signup',
         element: <SignupPage />,
       },
     ];
-
     const router = createMemoryRouter(routes, {
-      initialEntries: ["/signup"],
+      initialEntries: ['/signup'],
       initialIndex: 0,
     });
-
     render(
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
     );
   });
+  test('비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메시지가 표시된다.', async () => {
+    //given - 회원가입 페이지가 그려짐
 
-  test("비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러메세지가 표시된다", async () => {
-    // given - 회원가입 페이지가 그려짐
+    //현재 SignupPage에서 react-route-domd이랑 react-query를 쓰고 있는데, 실제 페이지 라우팅을 설정하는 것처럼 provider들로 다 감싸줘야 한다.
+    //when - 비밀번호와 비밀번호 확인값이 일치하지 않음
+    //이벤트를 발생시켜서 비밀번호 인풋고 비밀번호 확인 인풋에 값을 넣어줄 수 있음
 
-    // when - 비밀번호와 비밀번호 확인 값이 일치하지 않음
+    const passwordInput = screen.getByLabelText('비밀번호');
+    const confirmPasswordInput = screen.getByLabelText('비밀번호 확인');
 
-    const passwordInput = screen.getByLabelText("비밀번호");
-    const confirmPasswordInput = screen.getByLabelText("비밀번호 확인");
-
-    fireEvent.change(passwordInput, { target: { value: "password" } });
+    fireEvent.change(passwordInput, { target: { value: 'password' } });
     fireEvent.change(confirmPasswordInput, {
-      target: { value: "wrongPassword" },
+      target: { value: 'wrongPassword' },
     });
 
-    // then - 에러메세지가 표시됨
-    const errorMessage = await screen.findByTestId("error-message");
-    expect(errorMessage).toBeInTheDocument();
+    //then - 에러메시지가 표시됨
+
+    const errorMessage = await screen.findByTestId('error-message');
+    expect(errorMessage);
   });
 
-  test("이메일을 입력하고, 비밀번호와 비밀번호 확인값이 일치하면 회원가입 버튼이 활성화된다", async () => {
-    // given - 회원가입 페이지가 그려짐
-
-    const signupButton = screen.getByRole("button", { name: "회원가입" });
+  test('이메일을 입력하고, 비밀번호와 비밀번호 확인값이 일치하면 회원가입 버튼이 활성화 된다!', () => {
+    //given -> 회원가입 페이지가 그려짐
+    const signupButton = screen.getByRole('button', { name: '회원가입' });
     expect(signupButton).toBeDisabled();
 
-    // when - 이메일 입력, 비밀번호, 비밀번호 확인 일치
-    const emailInput = screen.getByLabelText("이메일");
-    const passwordInput = screen.getByLabelText("비밀번호");
-    const confirmPasswordInput = screen.getByLabelText("비밀번호 확인");
+    //when -> 이메일 입력, 비밀번호, 비밀번호 확인 일치
+    const emailInput = screen.getByLabelText('이메일');
+    const passwordInput = screen.getByLabelText('비밀번호');
+    const confirmPasswordInput = screen.getByLabelText('비밀번호 확인');
 
-    fireEvent.change(emailInput, {
-      target: { value: "button-activated@email.com" },
-    });
-    fireEvent.change(passwordInput, { target: { value: "password" } });
-    fireEvent.change(confirmPasswordInput, {
-      target: { value: "password" },
-    });
+    fireEvent.change(passwordInput, { target: { value: '비밀번호' } });
+    fireEvent.change(confirmPasswordInput, { target: { value: '비밀번호' } });
+    fireEvent.change(emailInput, { target: { value: 'lsy_0906@naver.com' } });
 
-    // then - 회원가입 버튼 활성화
+    //then -> 회원가입 버튼 활성화
+
     expect(signupButton).toBeEnabled();
   });
 });
