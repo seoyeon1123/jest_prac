954b4955e06477f602eabb8a848287f33d5d87a8
이서연 <iseoyeon@pyowonsig-ui-MacBookAir.local>
2025-01-16 16:05:54 +0900

    Feat: cypress prac

--- cypress/e2e/2-login/login.cy.js
+++ cypress/e2e/2-login/login.cy.js
@@ -1,31 +1,32 @@
-describe("로그인 화면", () => {
-  it("사용자는 아이디와 비밀번호를 사용해서 로그인한다", () => {
-    // given - 로그인 페이지에 접근한다
-    cy.visit("/login");
+const { should } = require('chai');
 
-    cy.get("[data-cy=emailInput]").as("emailInput");
-    cy.get("[data-cy=passwordInput]").as("passwordInput");
+describe('로그인 화면', () => {
+  //test 사용 불가능 무조건 it을 써야함
+  it('사용자는 아이디와 비밀번호를 사용해서 로그인해야 한다', () => {
+    //given - 로그인 페이지에 접근한다
 
-    // when - 아이디와 비밀번호를 입력하고 로그인 버튼을 클릭한다
-    cy.get("@emailInput").type("test@email.com");
-    cy.get("@passwordInput").type("password");
+    cy.visit('/login');
+    cy.get('[data-cy=emailInput').as('emailInput');
+    cy.get('[data-cy=passwordInput').as('passwordInput');
 
-    cy.get("@emailInput").invoke("val").should("eq", "test@email.com");
-    cy.get("@passwordInput").invoke("val").should("eq", "password");
+    //when - 아이디와 비밀번호를 입력하고 로그인 버튼을 클릭한다
+    cy.get('@emailInput').type('test@email.com');
+    cy.get('@passwordInput').type('password');
+
+    cy.get('@emailInput').invoke('val').should('eq', 'test@email.com');
+    cy.get('@passwordInput').invoke('val').should('eq', 'password');
 
     cy.intercept(
       {
-        method: "POST",
-        url: "/user/login",
+        method: 'POST',
+        url: '/user/login',
       },
-      { token: "AUTH_TOKEN" }
-    ).as("login");
+      { token: 'AUTH_TOKEN' }
+    );
 
-    cy.get("[data-cy=loginButton]").should("exist").click();
-    // then - 로그인에 성공하고 메인화면으로 이동한다
-    cy.url().should((url) => {
-      const currentUrl = new URL(url);
-      expect(currentUrl.pathname).to.equal("/");
-    });
+    cy.get('[data-cy=loginButton').should('exist').click();
+
+    //then - 로그인에 성공하고 메인화면으로 이동한다
+    cy.url().should('include', 'http://localhost:5173/');
   });
 });
